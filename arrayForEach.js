2ab8ac267142428ccc131f640ba9eb147c9b49fc
MaxKir <swat0505@gmail.com>
2025-03-04 13:06:49 +1000

    fd

--- arrayForEach.js
+++ arrayForEach.js
@@ -5,3 +5,41 @@ const numbers = [1,2,3,4,5]
 numbers.forEach((num) => {
     console.log(num * 2)
 })
+
+const fruits = ["apple", "banana", "lemone", "juice"]
+
+fruits.forEach((fruit, index) => {
+    console.log(`${index + 1}: ${fruit.toLowerCase()}`)
+})
+
+const users = [
+    { name: "Анна", age: 25 },
+    { name: "Борис", age: 30 },
+    { name: "Василий", age: 35 },
+    { name: "Олег", age: 41}
+];
+
+users.forEach((user, index) => user.age += 2);
+users.forEach((user, index) => {
+    console.log(`${index + 1}: ${user.name}, возраст: ${user.age}`);
+
+});
+
+// Подсчет суммы элементов
+
+const prises = [10,150,20,30, 70];
+let total = 0;
+
+prises.forEach((prise) => total += prise)
+console.log(total);
+
+// const prises = [10,150,20,30,70]
+// let total = 0;
+
+// for(let i = 0; i < prises.length; i++){
+//     total += prises[i];
+// }
+// console.log(total)
+
+
+
