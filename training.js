8df3f487430fd0a121804bc388f2b4fa428ec01f
MaxKir <swat0505@gmail.com>
2025-03-19 12:32:09 +1000

    fdsfds

--- training.js
+++ training.js
@@ -21,27 +21,27 @@
 // Все числа умножены на 2.
 // Оставлены только числа, которые больше 10.
 
-const numbers = [1,2,3,4,5,6,7]
+const numbers = [1, 2, 3, 4, 5, 6, 7]
 const res = numbers.map((num) => num * 2)
-.filter((num) => num > 10)
+  .filter((num) => num > 10)
 console.log(res)
 
 // Задание:
 // Дан массив чисел. Используя filter и reduce, посчитай сумму всех четных чисел.
 
-const numbers2 = [1,2,3,4,5,6,7,8]
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8]
 const res2 = numbers2.filter((num2) => num2 % 2 === 0)
-.reduce((sum, num2) => sum + num2)
+  .reduce((sum, num2) => sum + num2)
 console.log(res2);
 
 // Задание:
 // Дан массив чисел. Используя some, проверь, есть ли в массиве хотя бы одно отрицательное число.
-const numbers3 = [1,3,4,-6,7,-8]
+const numbers3 = [1, 3, 4, -6, 7, -8]
 console.log(numbers3.some((num) => num < 0))
 
 // Дан массив чисел. Используя every, проверь, являются ли все числа положительными.
 
-const numbers4 = [1,3,4,-6,7,-8]
+const numbers4 = [1, 3, 4, -6, 7, -8]
 console.log(numbers4.every((num) => num < 0))
 
 // Задание:
@@ -53,21 +53,21 @@ console.log(names)
 
 // Дан массив объектов с пользователями. Используя map, создай новый массив, содержащий только имена пользователей.
 const users = [
-    { id: 1, name: "Андрей", age: 25 },
-    { id: 2, name: "Виктория", age: 30 },
-    { id: 3, name: "Екатерина", age: 22 }
-  ];
-  const usersName = users.map((user) => user.name)
-  console.log(usersName)
+  { id: 1, name: "Андрей", age: 25 },
+  { id: 2, name: "Виктория", age: 30 },
+  { id: 3, name: "Екатерина", age: 22 }
+];
+const usersName = users.map((user) => user.name)
+console.log(usersName)
 
 //   Задание:
 // Дан массив товаров с ценами. Используя reduce, вычисли среднюю стоимость товара.
 
 const products = [
-    { name: "Телефон", price: 20000 },
-    { name: "Ноутбук", price: 50000 },
-    { name: "Планшет", price: 30000 }
-  ];
+  { name: "Телефон", price: 20000 },
+  { name: "Ноутбук", price: 50000 },
+  { name: "Планшет", price: 30000 }
+];
 const sumPrice = products.reduce((sum, products) => sum + products.price, 0)
 console.log(sumPrice)
 const averagePrice = sumPrice / products.length
@@ -76,19 +76,19 @@ console.log(`Средняя стоимость продуктов: ${averagePric
 // Задание:
 // Дан массив объектов с пользователями. Используя filter, получи массив только тех пользователей, которым больше 18 лет.
 const users2 = [
-    { name: "Алексей", age: 18 },
-    { name: "Мария", age: 20 },
-    { name: "Ольга", age: 15 },
-    { name: "Иван", age: 22 }
-  ];
+  { name: "Алексей", age: 18 },
+  { name: "Мария", age: 20 },
+  { name: "Ольга", age: 15 },
+  { name: "Иван", age: 22 }
+];
 const userAdult = users2.filter((user) => user.age >= 18)
 console.log(userAdult)
 
 // Дан массив имен. Используя forEach, выведи каждое имя в консоль.
 
 const names5 = ["Андрей", "Виктор", "Екатерина"];
-names5.sort((a,b) => b.localeCompare(a))
-.forEach((name) => console.log(name))
+names5.sort((a, b) => b.localeCompare(a))
+  .forEach((name) => console.log(name))
 
 // Дан массив пользователей. Найдите всех пользователей старше 18 лет, которые интересуются "спортом".
 
@@ -132,7 +132,7 @@ console.log(hasExcellent)
 
 const numbers5 = [1, 2, 3, 4, 2, 5, 1, 6, 3, 7];
 const uniqueNumbers = numbers5.filter((num, index, arr) => arr.indexOf(num) === index)
-console.log(uniqueNumbers) 
+console.log(uniqueNumbers)
 
 // Дан массив товаров с категориями. Группируйте товары по категориям.
 
@@ -145,9 +145,118 @@ const items = [
 ];
 
 const groupItems = items.reduce((acc, item) => {
-acc[item.category] = acc[item.category] || [];
-acc[item.category].push(item.name); 
-return acc
+  acc[item.category] = acc[item.category] || [];
+  acc[item.category].push(item.name);
+  return acc
 }, {})
 
 console.log(groupItems)
+
+// Напишите однострочное решение, которое вычисляет сумму квадратных корней для всех чётных чисел целочисленного массива.
+const numsA = [1, 4, 5, 16, 36, 45]
+const numsB = numsA.filter((num) => num % 2 === 0)
+  .reduce((acc, num) => acc + Math.sqrt(num), 0)
+console.log(numsB)
+
+// Написать функцию, которая принимает два числа и возращает результат их умножения
+
+function multiply(a, b) {
+  return a * b
+}
+
+console.log(multiply(5, 4))
+
+// Написать функцию, которой передаем, имя, фамилия и возраст, и получаем строку "Привет Иван Петров с возрастом 17 лет" 
+// (только здесь данные, которые были переданы в функцию)
+
+function userInfo(firstName, lastName, age) {
+  console.log(`Привет ${firstName} ${lastName} с возрастом ${age} лет`)
+}
+userInfo("Max", "Kirichko", 29)
+
+// Написать функцию, которая принимает пол человека ('M','F') в виде строки, результат функции возвращает строку "Ваш пол мужской" (или женский) 
+// или же "Ваш пол не опеределен"
+
+function userMale(male) {
+  if (male === 'M') {
+    console.log('Ваш пол мужской')
+  }
+  else if (male === 'F') {
+    console.log('Ваш пол женский')
+  }
+  else {
+    console.log('Ваш пол не определен')
+  }
+}
+
+userMale('5')
+
+// Сделайте функцию, которая принимает параметром число от 1 до 7, 
+// а возвращает день недели на русском языке.
+
+function dayOfWeek(num) {
+  const days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+  return num >= 1 && num <= 7 ? days[num - 1] : "Некорректный ввод";
+}
+console.log(dayOfWeek(1))
+console.log(dayOfWeek(9))
+
+// Написать функцию, которой передаем имя, и она возраващает приветствие в зависимости от времени суток 
+// (Доброе утро\день\вечер\ночи Иван)
+
+function greetUser(name) {
+  const hour = new Date().getHours();
+  let greeting;
+  if (hour >= 5 && hour <= 12) {
+    greeting = 'Доброе утро!'
+  }
+  else if (hour >= 13 && hour <= 18) {
+    greeting = 'Добрый день!'
+  }
+  else if (hour >= 19 && hour <= 24) {
+    greeting = 'Добрый день!'
+  }
+  else {
+    greeting = 'Доброй ночи!'
+  }
+return `${greeting} ${name}`
+
+}
+
+console.log(greetUser("Иван"))
+
+// Вывести числа от 1 до 100 в столбец. К каждой цифре подписать состояние возраста 
+// (1-17 ребенок, 18-30 - молодой, 30-55 - зрелый, от 55 - старый). Например. 33 - зрелый
+
+function getCategory(age){
+  if(age >= 1 && age <= 17){
+    return 'ребенок'
+  }
+  else if(age >= 18 && age <= 30){
+    return 'молодой'
+  }
+  else if(age >= 31 && age <= 55){
+    return 'зрелый'
+  }
+  else{
+    return 'старый'
+  }
+}
+
+function printAge(){
+  for (let i = 1; i <= 100; i++){
+    console.log(`${i} - ${getCategory(i)}`)
+
+  }
+}
+
+printAge()
+
+// Создай новую функцию, в которую передаешь имя и возраст человека и получаешь сообщение (Иван имеет возраст 44 и он зрелый). 
+// А также вызови внутри своей функции, функцию из прошлого задания
+
+function infUser(name, age){
+  return `${name} имеет возраст ${age} и он ${getCategory(age)}`
+}
+
+console.log(infUser('Max', 3))
