b74b550d85c0eccfb93ba623550e420b4d3f5238
MaxKir <swat0505@gmail.com>
2025-03-21 00:39:40 +1000

    fdsfds

---
+++ codewars.js
@@ -0,0 +1,17 @@
+// speed of aircrafts is given in *knots*
+// travelTime is in *minutes*
+// travel distance should be returned in *kilometers*
+function travelDistance(avgSpeed, travelTime) {
+    const travelHours = travelTime / 60;
+    const avgSpeedKm = avgSpeed * 1.852;
+    const travelKms   = avgSpeedKm * travelHours; 
+    
+    return travelKms;
+    
+  }
+
+  console.log(travelDistance(600, 60))
+  
+
+//   In one hour of sub-sonic flight at 600 knots should travel ~1111.2 km"
+// "In one hour of super-sonic flight at 800 knots should travel ~1481.6 km"
