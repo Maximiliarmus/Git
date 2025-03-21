bae87ebd9dbf410bc1f6fca5abb39906a8d453ee
MaxKir <swat0505@gmail.com>
2025-03-21 21:45:27 +1000

    111

--- codewars.js
+++ codewars.js
@@ -4,14 +4,74 @@
 function travelDistance(avgSpeed, travelTime) {
     const travelHours = travelTime / 60;
     const avgSpeedKm = avgSpeed * 1.852;
-    const travelKms   = avgSpeedKm * travelHours; 
-    
-    return travelKms;
-    
-  }
+    const travelKms = avgSpeedKm * travelHours;
 
-  console.log(travelDistance(600, 60))
-  
+    return travelKms;
+
+}
+
+console.log(travelDistance(600, 60))
+
 
 //   In one hour of sub-sonic flight at 600 knots should travel ~1111.2 km"
 // "In one hour of super-sonic flight at 800 knots should travel ~1481.6 km"
+
+// Вам даны два вектора, исходящие из начала координат (x=0, y=0) 
+// с координатами (x1,y1) и (x2,y2). Ваша задача — выяснить, являются 
+// ли эти векторы коллинеарными. Коллинеарные векторы — это векторы, 
+// которые лежат на одной прямой. Они могут быть направлены в одном и 
+// том же или противоположном направлении. Один вектор можно получить из 
+// другого, умножив его на определенное число. В терминах координат 
+// векторы (x1, y1) и (x2, y2) являются коллинеарными, если (x1, y1) = (k*x2, k*y2), 
+// где k — любое число, выступающее в качестве коэффициента.
+
+function collinearity(x1, y1, x2, y2) {
+    if(x1 * y2 - x2 * y1 === 0) return true
+    else return false
+}
+
+
+// assert.strictEqual(collinearity(1, 1, 1, 1), true, `collinearity( 1,1, 1,1 )`);
+// assert.strictEqual(collinearity(1, 2, 2, 4), true, `collinearity( 1,2, 2,4 )`);
+// assert.strictEqual(collinearity(1, 2, -1, -2), true, `collinearity( 1,2, -1,-2 )`);
+// assert.strictEqual(collinearity(1, 1, 6, 1), false, `collinearity( 1,1, 6,1 )`);
+// assert.strictEqual(collinearity(1, 2, 1, -2), false, `collinearity( 1,2, 1,-2 )`);
+// assert.strictEqual(collinearity(4, 0, 11, 0), true, `collinearity( 4,0, 11,0 )`);
+// assert.strictEqual(collinearity(0, 1, 6, 0), false, `collinearity( 0,1, 6,0 )`);
+// assert.strictEqual(collinearity(4, 4, 0, 4), false, `collinearity( 4,4, 0,4 )`);
+// assert.strictEqual(collinearity(0, 0, 0, 0), true, `collinearity( 0,0, 0,0 )`);
+// assert.strictEqual(collinearity(0, 0, 1, 0), true, `collinearity( 0,0, 1,0 )`);
+// assert.strictEqual(collinearity(5, 7, 0, 0), true, `collinearity( 5,7, 0,0 )`);
+
+// Нам нужна функция, которая может преобразовать число (целое число) в строку.
+
+// Какие способы достижения этого вы знаете?
+
+function numberToString(num) {
+    if(typeof num === 'number') return num.toString()
+  }
+console.log(numberToString(4))  
+
+// ("+-+", "+--") ➞ "+-0"
+// # Compare the first characters of each string, then the next in turn.
+// # "+" against a "+" returns another "+".
+// # "-" against a "-" returns another "-".
+// # "+" against a "-" returns "0".
+// # Return the string of characters.
+
+function neutralise(s1, s2) {
+    let arr = [];
+    for(let i = 0; i < s1.length; i++){
+        if(s1[i] === "+" && s2[i] === "+"){
+            arr.push("+")
+        }
+        else if(s1[i] === "-" && s2[i] === "-" ) {
+            arr.push("-")
+        }
+            
+        else {arr.push("0")}
+    }
+    let result2 = arr.join("");
+    return result2;
+  }
+  console.log(neutralise(("--++--"), ("--+++-")))
