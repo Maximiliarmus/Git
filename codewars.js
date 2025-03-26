b483d52d8dc877eed920f4464df72a2d0435522a
MaxKir <swat0505@gmail.com>
2025-03-26 13:51:34 +1000

    111

--- codewars.js
+++ codewars.js
@@ -219,8 +219,62 @@ console.log(boolToWord(false))
 // возвращать значение true, если они влюблены, и false, если нет.
 
 function loveFunc(flower1, flower2) {
-    if(flower1 % 2 === 0 && flower2 % 2 !== 0) return true
-    else if(flower1 % 2 !== 0 && flower2 % 2 === 0) return true
+    if (flower1 % 2 === 0 && flower2 % 2 !== 0) return true
+    else if (flower1 % 2 !== 0 && flower2 % 2 === 0) return true
     else return false
 }
-console.log(loveFunc(0,1))
+console.log(loveFunc(0, 1))
+
+// Для заданной точки на евклидовой плоскости ( x и y) вернуть квадрант, в котором находится точка: 1, 2, 3 
+// или 4(целое число). 
+// x и y являются ненулевыми целыми числами, поэтому заданная точка никогда не лежит на осях.
+
+function quadrant(x, y) {
+    if (x > 0 && y > 0) return 1
+    else if (x < 0 && y > 0) return 2
+    else if (x < 0 && y < 0) return 3
+    else return 4
+}
+console.log(quadrant(1, -2))
+
+// Часы показывают h часы, m минуты и s секунды после полуночи.
+
+// Ваша задача — написать функцию, которая возвращает время с полуночи в миллисекундах.
+
+function past(h, m, s) {
+    if (h < 0 || h > 23) throw new Error("Неверно указаны часы");
+    if (m < 0 || m > 59) throw new Error("Неверно указаны минуты");
+    if (s < 0 || s > 59) throw new Error("Неверно указаны секунды");
+    timePerMil = (h * 3600000) + (m * 60000) + (s * 1000)
+    return timePerMil
+}
+console.log(past(0, 0, 0))
+
+// Получаете массив чисел, возвращаете сумму всех положительных единиц.
+function positiveSum(nums) {
+  const resNums = nums.filter((num) => num > 0)
+  .reduce((sum, num) => sum + num, 0)
+  return resNums
+}
+console.log(positiveSum([-1, -4, -7, -12]))
+
+// Дана строка цифр, вам следует заменить любую цифру ниже 5 на '0', 
+// а любую цифру 5 и выше на '1'. Верните полученную строку.
+
+function fakeBin(x){
+const arrNums = x.split("").map(Number);
+const arrRes = []   
+for(let i = 0; i < arrNums.length; i++){
+    
+    if(arrNums[i] < 5){
+        arrRes.push(0)
+    }
+    else {
+        arrRes.push(1)
+    }
+    
+}
+ const arrRes2 = arrRes.map(String).join('');
+return arrRes2 
+}
+console.log(fakeBin("45385593107843568"))
